2d764a7171b398205216120d6581e5b63ed1a9e7
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-15 21:07:44 +0530

    commited

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div >
+    <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>

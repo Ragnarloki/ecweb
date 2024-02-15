48be78d5ec8a31bbbcacf11f60b31df325ce8786
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-15 20:56:25 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "jquery/dist/jquery.min.js"
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 function App() {
   return (
     <div>
-      <button className='btn btn-primary'>react</button>
-    </div>
+      <Navbar/>
+       </div>
   )
 }
 

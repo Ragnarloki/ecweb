d0a37330210d6f408d4f51e49f368e1986680d47
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-17 13:47:57 +0530

    commited

---
+++ src/content.js
@@ -0,0 +1,14 @@
+const fruits=[{
+    id:1,
+    fruitname:"pomegranate",
+    img:"https://insanelygoodrecipes.com/wp-content/uploads/2022/12/Fresh-and-Orgaic-Pink-Pomegranate.jpg"
+},
+{
+    id:2,
+    fruitname:"apple",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFo_Il63ryN-cT1KOd-K-JNZS-ju0bm3eBlA&usqp=CAU"
+},{
+    id:3,
+    fruitname:'Banana',
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXnnjIIC_3f58zUMU18rAjMg-eq8spG7UNXA&usqp=CAU"
+}]

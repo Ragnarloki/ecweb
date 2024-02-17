25e2f554b25a3511d63fecd0ad6eb3ebfc2131ca
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-17 11:29:54 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navbar from './components/Navbar/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "jquery/dist/jquery.min.js"
 import 'bootstrap/dist/js/bootstrap.min.js'
+import Card from './components/Card'
 
 function App() {
   return (
     <div>
       <Navbar/>
-       </div>
+      <Card />
+    </div>
   )
 }
 

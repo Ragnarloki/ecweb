8df9a6299bc4ac027aaf05512e4132ebf6115f97
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-18 12:46:13 +0530

    commited

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,22 @@ import './Card.css'
 
 function Card(props) {
   return (
-    <div className='body'>
-    <div className="row">
-      <div className="column">
+    
+    <div className="container">
+      <div className='row'>
+        <div className='col-md-3'>
+
         <div className=" card ">
-         <img src={props.image} className="card-img-top" />
+         <img src={props.image} className="card-img-top" height={'200px'} width={'170px'}  />
              <div className="card-body">
                <h5 className="card-title justify d-flex justify-content-center">{props.name}</h5>
               
              </div>
+        
+             </div>
+      </div>
         </div>
       </div>
-  </div>
-  </div>
   )
 }
 

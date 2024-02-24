97c0402ae3818ce22b957b35b1a65cc5dec63e24
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-24 11:49:44 +0530

    commited

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">siva</a>
+    <a className="navbar-brand" href="#">shop-card</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>

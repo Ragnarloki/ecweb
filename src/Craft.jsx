1aa740ac49db08908adf9e7fc0e579850ea3ebe2
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-24 17:21:45 +0530

    commited

--- src/Craft.jsx
+++ src/Craft.jsx
@@ -10,10 +10,10 @@ function Craft() {
        
         <div className="container-fluid">
   <div className="row">
-    <div className="col-4">
+    <div className="col-sm-4">
       <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/q/h/q/-original-imagg7t2bjudx78m.jpeg?q=70" alt="" />
     </div>
-    <div className="col-8">
+    <div className="col-sm-8">
       2 of 3 (wider)
     </div>
   </div>

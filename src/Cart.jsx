7a3513142f657a1c0ebbb1870c0f59e5fe51f2e3
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-03-01 12:44:12 +0530

    adding cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,7 +3,11 @@ import React, { useState } from 'react'
 function Cart({cart,setCart}){
 
  const [price,setprice]=useState(0);
+ const [count,setCount]=useState();
 
+ function buttonClicked(){
+
+ }
 
 
 
@@ -19,6 +23,9 @@ function Cart({cart,setCart}){
   <div className="card-body">
     <h5 className="card-title d-flex justify-content-center">{item.fruitname}</h5>
     <h3 className='cart-title d-flex justify-content-center'>{item.price}</h3>
+    <div className='cart-title d-flex justify-content-center justify-content-evenly'>
+    <button className='button' onClick={minus}>-</button><h3 className='d-flex '> Add </h3><button onClick={plus} className='button'>+</button>
+    </div> 
   </div>
 </div>
 
